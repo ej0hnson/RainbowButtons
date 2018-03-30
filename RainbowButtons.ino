b375f96cf13f0eef6d2a988d539382b51de15a37
ej0hnson <elliottljohnson@gmail.com>
2018-03-29 17:11:43 -0700

    Scrolling rainbows for OCTOWS2811 on Teensy
    Scrolling rainbows for OCTOWS2811 on Teensy

---
+++ RainbowButtons.ino
@@ -0,0 +1,77 @@
+#include <OctoWS2811.h>
+
+const int ledsPerStrip = 24;
+
+DMAMEM int displayMemory[ledsPerStrip*6];
+int drawingMemory[ledsPerStrip*6];
+int rainbowColors[180];
+
+const int config = WS2811_RGB | WS2811_800kHz;
+
+OctoWS2811 leds(ledsPerStrip, displayMemory, drawingMemory, config);
+
+unsigned int h2rgb(unsigned int v1, unsigned int v2, unsigned int hue)
+{
+  if (hue < 60) return v1 * 60 + (v2 - v1) * hue;
+  if (hue < 180) return v2 * 60;
+  if (hue < 240) return v1 * 60 + (v2 - v1) * (240 - hue);
+  return v1 * 60;
+}
+
+int makeColor(unsigned int hue, unsigned int saturation, unsigned int lightness)
+{
+  unsigned int red, green, blue;
+  unsigned int var1, var2;
+
+  if (hue > 359) hue = hue % 360;
+  if (saturation > 100) saturation = 100;
+  if (lightness > 100) lightness = 100;
+
+  // algorithm from: http://www.easyrgb.com/index.php?X=MATH&H=19#text19
+  if (saturation == 0) {
+    red = green = blue = lightness * 255 / 100;
+  } else {
+    if (lightness < 50) {
+      var2 = lightness * (100 + saturation);
+    } else {
+      var2 = ((lightness + saturation) * 100) - (saturation * lightness);
+    }
+    var1 = lightness * 200 - var2;
+    red = h2rgb(var1, var2, (hue < 240) ? hue + 120 : hue - 240) * 255 / 600000;
+    green = h2rgb(var1, var2, hue) * 255 / 600000;
+    blue = h2rgb(var1, var2, (hue >= 120) ? hue - 120 : hue + 240) * 255 / 600000;
+  }
+  return (red << 16) | (green << 8) | blue;
+}
+
+void setup() {
+    for (int i=0; i<180; i++) {
+     int hue = i * 2;
+     int saturation = 100;
+     int lightness = 50;
+     // pre-compute the 180 rainbow colors
+     rainbowColors[i] = makeColor(hue, saturation, lightness);
+  }
+  leds.begin();
+}
+
+void rainbow(int phaseShift, int cycleTime)
+{
+  int color, x, y, wait;
+
+  wait = cycleTime * 1000 / ledsPerStrip;
+  for (color=0; color < 180; color++) {
+    for (x=0; x < ledsPerStrip; x++) {
+      for (y=0; y < 8; y++) {
+        int index = (color + x + y*phaseShift/2) % 180;
+        leds.setPixel(x + y*ledsPerStrip, rainbowColors[index]);
+      }
+    }
+    leds.show();
+    delayMicroseconds(wait);
+  }
+}
+
+void loop() {
+  rainbow(10, 1000);  
+}
